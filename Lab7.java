7589e9e6275476f8ffdd7254f541430c29905b5c
CPaulMorrey <56007942+CPaulMorrey@users.noreply.github.com>
2020-10-13 15:31:11 -0600

    Add files via upload

---
+++ Lab7.java
@@ -0,0 +1,34 @@
+// Lab7.java
+// Build command: javac Lab7.java
+
+public class Lab7 {
+    public static void main(String[] args) {
+        System.out.println("IT 6780 - Lab 7");
+        System.out.println("The IT 6780 class is filled with many awesome programmers.  Some of them are:");
+
+// guides.github.com/activities/hello-world
+
+// Instructions: Remove the two forward slashes (comment marker) on the line with your name.
+//               Test the program to make sure it prints your name (if possible).
+//               Commit your changes.
+//               Open a pull request.
+//               After the rest of the class has arrived at this point then all names should be listed by the running program.
+
+//        System.out.println("Judy");
+//        System.out.println("Landon");
+//        System.out.println("TJ");
+//        System.out.println("Imer");
+//        System.out.println("Conrad");
+//        System.out.println("Alan");
+//        System.out.println("Kyle");
+//        System.out.println("Cesar");
+//        System.out.println("Paul");
+//        System.out.println("Oz");
+//        System.out.println("Brent");
+//        System.out.println("Lacey");
+//        System.out.println("Walker");
+//        System.out.println("Chicco");
+//        System.out.println("Ilia");
+//        System.out.println("Jose");
+    }
+}
