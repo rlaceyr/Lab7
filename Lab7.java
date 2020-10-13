7e5a35f4bf9cf96be8d5875f7999eda65dff8ef0
CPaulMorrey <56007942+CPaulMorrey@users.noreply.github.com>
2020-10-13 16:58:17 -0600

    Update Lab7.java

--- Lab7.java
+++ Lab7.java
@@ -6,13 +6,37 @@ public class Lab7 {
         System.out.println("IT 6780 - Lab 7");
         System.out.println("The IT 6780 class is filled with many awesome programmers.  Some of them are:");
 
-// guides.github.com/activities/hello-world
+/*
 
-// Instructions: Remove the two forward slashes (comment marker) on the line with your name.
-//               Test the program to make sure it prints your name (if possible).
-//               Commit your changes.
-//               Open a pull request.
-//               After the rest of the class has arrived at this point then all names should be listed by the running program.
+Command line version of the activity
+------------------------------------
+BEFORE YOU START... you need to have git installed on your system.
+
+Instructions: 
+               Clone a local repository to your system.
+                    git clone https://github.com/CPaulMorrey/Lab7.git
+                    cd Lab7
+                    
+               Test the program
+                    javac Lab7.java
+                    java Lab7
+                    
+               Edit the source code of the program.  Remove the two forward slashes (comment marker) on the line with your name.
+               
+               Test the program to make sure it prints your name (if possible).
+                    javac Lab7.java
+                    java Lab7
+                    git diff
+               
+               Commit your changes.
+                    git commit Lab7.java -m "Added myself"
+                    git push https://github.com/CPaulMorrey/Lab7.git 
+               
+               After the rest of the class has arrived at this point then all names should be listed by the running program.
+               Update your local repository and it should pull in the changes that other students have made.
+                    git fetch
+               
+*/
 
 //        System.out.println("Judy");
 //        System.out.println("Landon");
